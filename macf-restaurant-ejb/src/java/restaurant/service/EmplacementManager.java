fbbc117ea4c5801f1d6fb12192ab4fad95f7cdb3
Marine <marineg0108@gmail.com>
2016-12-14 15:57:03 +0100

    code metier

--- macf-restaurant-ejb/src/java/restaurant/service/EmplacementManager.java
+++ macf-restaurant-ejb/src/java/restaurant/service/EmplacementManager.java
@@ -2,16 +2,25 @@ package restaurant.service;
 
 import java.util.List;
 import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import restaurant.model.salle.Emplacement;
 import restaurant.model.salle.Zone;
+import restaurant.repository.EmplacementRepository;
 
 @Stateless(name = "emplacement-manager")
 public class EmplacementManager {
     private static final Logger LOG = Logger.getLogger(EmplacementManager.class.getName());
     
     @EJB
+    private EmplacementRepository repository;
+    
+    
+    @PostConstruct
+    public void construct(){
+    }
+    
     public Emplacement create(String numero, int nombrePlaces, String numeroZone) {
         throw new UnsupportedOperationException();
     }
