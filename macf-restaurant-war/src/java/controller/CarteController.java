d17eaed5df3885d0021f587e75612fb3460087f5
AmauryBrunel <amaury-21@hotmail.fr>
2016-12-15 15:18:32 +0100

    affichageParCatégorie

--- macf-restaurant-war/src/java/controller/CarteController.java
+++ macf-restaurant-war/src/java/controller/CarteController.java
@@ -9,36 +9,47 @@ import restaurant.model.catalogue.Ingredient;
 import restaurant.service.ArticleManagerInterface;
 import restaurant.service.MenuManagerInterface;
 
-public class CarteController extends ActionController{
-    
+public class CarteController extends ActionController {
+
     private static final Logger LOG = Logger.getLogger(CarteController.class.getName());
-    
+
     private final ArticleManagerInterface articleManager;
     private final MenuManagerInterface menuManager;
-    
-    public CarteController(){
+
+    public CarteController() {
         articleManager = get("java:global/macf-restaurant/macf-restaurant-ejb/article-manager");
         menuManager = get("java:global/macf-restaurant/macf-restaurant-ejb/menu-manager");
     }
-    
-    public String listArticle (HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+    public String afficherCarte(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        request.setAttribute("categories", Categorie.values());
+//        String cat = request.getParameter("choix");
+//        Categorie categorie = Categorie.valueOf(cat);
+//        request.setAttribute("articles", articleManager.getByCategorie(categorie));
+        return "article/list";
+    }
+
+    public String listArticle(HttpServletRequest request, HttpServletResponse response) throws Exception {
         request.setAttribute("articles", articleManager.getAll());
         return "article/list";
     }
-    
-    public String listArticleByCategorie (HttpServletRequest request, HttpServletResponse response, Categorie categorie) throws Exception {
-       request.setAttribute("articles", articleManager.getByCategorie(categorie));
+
+    public String listArticleByCategorie(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String cat = request.getParameter("categorie");
+        Categorie categorie = Categorie.valueOf(cat);
+        request.setAttribute("articles", articleManager.getByCategorie(categorie));
         return "article/list";
     }
-    
-    public String listArticleByIngredient (HttpServletRequest request, HttpServletResponse response, Ingredient ingredient) throws Exception {
-        request.setAttribute("articles", articleManager.getByIngredient(ingredient));
-        return "article/list";
-    }
-    
-    public String listMenu (HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+//    public String listArticleByIngredient (HttpServletRequest request, HttpServletResponse response) throws Exception {
+//        String ing = request.getParameter("ingredient");
+//        Ingredient ingredient = Ingredient.valueOf(ing);
+//        request.setAttribute("articles", articleManager.getByIngredient(ingredient));
+//        return "article/list";
+//    }
+    public String listMenu(HttpServletRequest request, HttpServletResponse response) throws Exception {
         request.setAttribute("articles", menuManager.getAll());
         return "article/list";
     }
-   
+
 }
